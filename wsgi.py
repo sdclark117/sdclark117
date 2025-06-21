35484696340a1748ad67c5d654d09511b86ffe60
sdclark117 <sdclark117@gmail.com>
2025-06-21 00:18:07 -0500

    fix: Clean up wsgi.py for proper gunicorn entry point

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-from waitress import serve
 from app import app, db
 
 # This file is the new entry point for the gunicorn server.
@@ -8,8 +7,4 @@ with app.app_context():
     db.create_all()
 
 # To run this application in a production environment, use:
-# gunicorn --bind 0.0.0.0:10000 wsgi:app
-
-if __name__ == '__main__':
-    print("Starting production server on http://localhost:8080")
-    serve(app, host='0.0.0.0', port=8080, threads=4) 
+# gunicorn --bind 0.0.0.0:10000 wsgi:app 
