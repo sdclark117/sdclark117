741add73301faa40c79f7f6c339adca080659e3f
sdclark117 <sdclark117@gmail.com>
2025-07-23 17:32:41 -0500

    Fix isort import sorting for migration file

--- migrations/add_guest_usage_table.py
+++ migrations/add_guest_usage_table.py
@@ -6,9 +6,8 @@ Create Date: 2024-01-01 12:00:00.000000
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "add_guest_usage_table"
