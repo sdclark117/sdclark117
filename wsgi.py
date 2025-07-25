6fac6641e6eadbb08de3f7aaa48bb242efa2047f
sdclark117 <sdclark117@gmail.com>
2025-07-25 03:37:56 -0500

    Format wsgi.py with black

--- wsgi.py
+++ wsgi.py
@@ -27,7 +27,9 @@ def init_database():
 if __name__ == "__main__":
     init_database()
     app.run(
-        host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False  # nosec B104
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        debug=False,  # nosec B104
     )
 
 
